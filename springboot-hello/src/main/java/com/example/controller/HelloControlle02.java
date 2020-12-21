98cfceccb4a1c2177751e2e65749a0fd3c0e56b2
liuxin <183771798@qq.com>
2020-12-21 08:49:54 +0800

    2020-12-21 08:49

--- springboot-hello/src/main/java/com/example/controller/HelloControlle02.java
+++ springboot-hello/src/main/java/com/example/controller/HelloControlle02.java
@@ -2,22 +2,26 @@ package com.example.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
-public class HelloController {
+public class HelloControlle02 {
 
     @RequestMapping("/index")
+    @ResponseBody
     public String index(){
-        return "Mayikt";
+        //转发到mayikt,如果不加@ResponseBody，访问时就会报错
+        return "mayikt";
     }
 
     public static void main(String[] args) {
         /**
          * 启动类入口class,默认整合tomcat，端口号8080
          */
-        SpringApplication.run(HelloController.class,args);
+        SpringApplication.run(HelloControlle02.class,args);
     }
 }
--- springboot-properties/src/main/java/com/example/properties/UserValue.java
+++ springboot-properties/src/main/java/com/example/properties/UserValue.java
@@ -2,7 +2,6 @@ package com.example.properties;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,3 +9,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@Data
+@Component
+public class UserValue {
+    @Value("${user.id}")
+    private Integer id;
+    @Value("${user.last-name}")
+    private String lastName;
+    @Value("${user.strs}")
+    private String[] strs;
+    @Value("${user.age}")
+    private Integer age;
+    @Value("${user.birth}")
+    private Date birth;
+    @Value("${user.lists}")
+    private List<String> lists;
+
+    @Value("${user.sets}")
+    private Set<String> sets;
+
+    /**
+     * 不支持复杂类型属性Map,person对象的获取
+     */
+    private Map<String,String> maps;
+    private Person person;
+}
